7fe35b60e67d03e845769e8ed2610d5cd3673f1f
Alexie Ortiz Falu <alexie_15@hotmail.com>
2017-11-02 15:14:44 -0400

    Installation of Route, Adding App Module Test

---
+++ Doc_Appointment/Angular_Module/app_test_mod.js
@@ -0,0 +1,21 @@
+﻿var App = angular.module("App", ['ngRoute']);
+
+App.config(['$routeprovider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/Schedule', {
+                TemplateUrl: 'Front_End/schedule.html',
+                controller: 'ScheduleController'
+            }).
+            when('/Appointments', {
+                TemplateUrl: 'Front_End/appointments.html',
+                controller: 'AppointmentsController'
+            }).
+            when('/Home', {
+                TemplateUrl: 'Front_End/home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/Home'
+            });
+    }]);
