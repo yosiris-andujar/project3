ca4b0b83ec5204ec22ee24dd391e1c60fc6e5471
Alexie Ortiz Falu <alexie_15@hotmail.com>
2017-11-02 22:15:57 -0400

    Adding schedule module, adding blank html of appointments and schedule

--- Doc_Appointment/Angular_Module/app_test_mod.js
+++ Doc_Appointment/Angular_Module/app_test_mod.js
@@ -1,4 +1,4 @@
-﻿var App = angular.module("App", ['ngRoute']);
+﻿var App = angular.module("App", ['ngRoute','ScheduleService']);
 
 App.config(['$routeprovider',
     function ($routeProvider) {
@@ -19,3 +19,57 @@ App.config(['$routeprovider',
                 redirectTo: '/Home'
             });
     }]);
+
+App.controller('ScheduleController', function ($scope, ScheduleApi){
+
+    getSchedules(1);
+    function getSchedules(role) {
+        ScheduleApi.getSchedules(role).success(function (schedule) {
+            $scope.schedule = schedule;
+        })
+            .error(function (error) {
+                $scope.status = 'Unable to load All Schedules Data: ' + error.message;
+            })
+    }
+
+    function getSchedule(id) {
+        SchudelApi.getSchedule(id).success(function (schedule) {
+            $scope.schedule = schedule;
+        })
+            .error(function (error) {
+                $scope.status = 'Unable to load Schedule Data: ' + error.message;
+            })
+    }
+
+    function insertSchedule(newSchedule) {
+        ScheduleApi.insertSchedule(newSchedule).success(function (schedule) {
+            $scope.status = 'Inserted Appointment! Refreshing appointment list.';
+            $scope.schedule.push(newSchedule);
+        })
+            .error(function (error) {
+                $scope.status = 'Unable to insert schedule: ' + error.message;
+            })
+    }
+
+    function edtiSchedule(newSchedule)
+
+    //function deleteSchedule(delSchedule) {
+    //    ScheduleApi.deleteSchedule(delSchedule).success(function (schedule) {
+    //        $scope.status = 'Deleted Schedule! Refreshing schedule list.';
+    //        for (var i = 0; i < l$scope.schedule.lenght; i++) {
+    //            var oldSchedule = $scope.schedule[i];
+    //        }
+    //    })
+    //}
+
+
+    
+});
+App.controller('AppointmentsController', function ($scope) {
+
+
+});
+App.controller('HomeController', function ($scope) {
+
+
+});
---
+++ Doc_Appointment/Angular_Module/schedule_mod.js
@@ -0,0 +1,41 @@
+﻿var ScheduleService = angular.module('ScheduleService', []);
+
+ScheduleService.factory('ScheduleApi', function ($http) {
+
+    var urlBase = '';
+    var ScheduleApi = {};
+
+    ScheduleApi.getSchedules = function (role) {
+        return $http.get(urlBase + '/Schedules' + role);
+    };
+
+    ScheduleApi.getSchedule = function (id) {
+        return $http.get(urlBase + '/Schedule' + id);
+    };
+
+    SheduleApi.insertSchedule = function (schedule) {
+
+        return $http.post(urlBase + '/Schedule/' + schedule);
+    };
+
+    ScheduleApi.deleteSchedule = function (delSchedule) {
+
+        var request = $http({
+            method: 'delete',
+            url: urlBase + '/Schedule/' + delSchedule.Id
+        });
+        return request;
+    };
+
+    ScheduleApi.editSchedule = function (editSchedule) {
+
+        var request = $http({
+            method: 'put',
+            url: urlBase + '/Schedule/' + editSchedule.Id,
+            data: editSchedule
+        });
+        return request;
+    };
+
+    return ScheduleApi;
+});
